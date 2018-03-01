99afcba8c3593da0de9615883c42786d4fbef774
Peter Wu <piterwu@outlook.com>
2018-03-01 09:14:10 +0800

    测试调整

--- plugin-publish/build.gradle.kts
+++ plugin-publish/build.gradle.kts
@@ -24,17 +24,6 @@ repositories {
 }
 dependencies {
     compile(gradleApi())
-    compile("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.7.3")
-    compile("com.gradle.publish:plugin-publish-plugin:0.9.7")
-    compile("org.jetbrains.dokka:dokka-gradle-plugin:0.9.15") {
-        exclude(group = "org.jetbrains.kotlin")
-    }
-
-    compileOnly("org.jfrog.buildinfo:build-info-extractor-gradle:4.5.4") {
-        exclude(module = "groovy-all")
-    }
-    testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.1.61")
-
 }
 
 val compileKotlin: KotlinCompile by tasks
---
+++ plugin-publish/src/main/kotlin/cn/bestwu/samples/gradle/test/TestPlugin.kt
@@ -0,0 +1,23 @@
+package cn.bestwu.samples.gradle.test
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+import sun.plugin2.main.server.AbstractPlugin
+
+/**
+ *
+ * 注册task
+ *
+ * @author Peter Wu
+ */
+class TestPlugin : Plugin<Project> {
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun apply(project: Project) {
+
+    }
+
+}
