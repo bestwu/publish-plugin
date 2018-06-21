604fe55ed0fec671f69eb0abfaf5011876a46b1b
Peter Wu <piterwu@outlook.com>
2018-06-21 17:58:03 +0800

    升级gradle;升级依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.2.30"
+val kotlinVersion = "1.2.50"
 
 plugins {
-    kotlin("jvm") version "1.2.30"
+    kotlin("jvm") version "1.2.50"
     id("cn.bestwu.plugin-publish")
 }
 
 group = "cn.bestwu.gradle"
-version = "0.0.19"
+version = "0.0.20"
 
 repositories {
     jcenter()
@@ -17,11 +17,11 @@ repositories {
 dependencies {
     compile(kotlin("reflect", kotlinVersion))
     compile(gradleApi())
-    compile("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.0")
-    compile("com.gradle.publish:plugin-publish-plugin:0.9.7")
-    compile("org.jetbrains.dokka:dokka-gradle-plugin:0.9.16")
+    compile("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.2")
+    compile("com.gradle.publish:plugin-publish-plugin:0.9.10")
+    compile("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
 
-    compileOnly("org.jfrog.buildinfo:build-info-extractor-gradle:4.6.2") {
+    compileOnly("org.jfrog.buildinfo:build-info-extractor-gradle:4.7.4") {
         exclude(module = "groovy-all")
     }
     testCompile("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
